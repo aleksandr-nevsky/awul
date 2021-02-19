58c873f694644d8e8bd016980a6d319a154cb6fc
Aleksandr Vvedensky <me@nevsky.cc>
2021-02-19 19:44:14 +0300

    First PWM.

---
+++ src/main/java/cc/nevsky/java/AwulController.java
@@ -0,0 +1,39 @@
+package cc.nevsky.java;
+
+import com.pi4j.wiringpi.SoftPwm;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/awul")
+public class AwulController {
+    /**
+     * Пин для ШИМ.
+     * Шестой сверху. № 12.
+     */
+    private static final int AWUL_PIN = 1;
+
+    static {
+        // initialize wiringPi library
+        com.pi4j.wiringpi.Gpio.wiringPiSetup();
+
+        // create soft-pwm pins (min=0 ; max=100)
+        SoftPwm.softPwmCreate(AWUL_PIN, 0, 100);
+    }
+
+    @Path("/pwm/{value}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String pwm(@PathParam int value) {
+        if (value >= 0 && value <= 100) {
+            SoftPwm.softPwmWrite(AWUL_PIN, value);
+            return "Set value = " + value;
+        } else {
+            return "Error. Use value 0..100.";
+        }
+    }
+
+}
---
+++ src/test/java/cc/nevsky/java/NativeAwulControllerIT.java
@@ -0,0 +1,9 @@
+package cc.nevsky.java;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeAwulControllerIT extends AwulControllerTest {
+
+    // Execute the same tests but in native mode.
+}
